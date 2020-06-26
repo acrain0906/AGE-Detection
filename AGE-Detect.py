b402115983271e204477095165d822fa6c253306
acrain0906 <aoc5329@gmail.com>
2020-06-26 01:50:16 -0700

    Modularized code
    divided code into logical blocks, to simplify code changes in the future, espeically as the code-base expands

--- AGE-Detect.py
+++ AGE-Detect.py
@@ -46,7 +46,7 @@ from twilio.rest import Client
 
 from sklearn.metrics import confusion_matrix
 
-# In[23]:
+
 
 
 def Summary(directory):
@@ -85,36 +85,29 @@ def Summary(directory):
 			break;
 	return pd.DataFrame(data)
 
-
 # In[24]:
 
+def importData(base_directory):
+	# Build Database
+	start = time.time()
+	# read images into list 
+	print('Summary 3...')
+	directory = base_directory + 'part3/'
+	# print(directory)
+	summary3 = Summary(directory)
 
+	print('Summary 2...')
+	directory = base_directory + 'part2/'
+	# summary2 = Summary(directory)
 
+	print('Summary 1...')
+	# directory = 'D:/Downloads/Faces/AWS Instance/data/part1/' windows
+	directory = base_directory + 'part1/'
+	# summary1 = Summary(directory)
+	print('time taken: {} minutes'.format((time.time() - start)/60))
 
-# Build Database
-start = time.time()
-# read images into list 
-print('Summary 3...')
-directory = './part3.tar/part3/'
-summary3 = Summary(directory)
-
-print('Summary 2...')
-directory = './part3.tar/part2/'
-# summary2 = Summary(directory)
-
-print('Summary 1...')
-# directory = 'D:/Downloads/Faces/AWS Instance/data/part1/' windows
-directory = './part3.tar/part2/'
-# summary1 = Summary(directory)
-print('time taken: {} minutes'.format((time.time() - start)/60))
-
-summary = summary3 # .append(summary2)
-
-
-# In[25]:
-
-
-detector = MTCNN()
+	summary = summary3 # .append(summary2)
+	return summary 
 
 # pixels = summary['picture'].values[200][0]
 def catch (file):
@@ -171,24 +164,16 @@ def extractFace (pixels):
 
 	return face_array
 
-
-# In[26]:
-
-
-print('cropping faces...')
-start = time.time()
-# create picture library
-pictures = {}
-for index, row in summary.iterrows():
-	if index % 1000 == 0:
-		print(index)
-	pictures[row['filename']] = extractFace (catch(row['filename']))
-print('time taken: {} minutes'.format((time.time() - start)/60))
-
-del detector
-K.clear_session()
-gc.collect()
-
+def cropFaces(summary):
+	print('cropping faces...')
+	start = time.time()
+	# create picture library
+	pictures = {}
+	for index, row in summary.iterrows():
+		if index % 1000 == 0:
+			print(index)
+		pictures[row['filename']] = extractFace (catch(row['filename']))
+	print('time taken: {} minutes'.format((time.time() - start)/60))
 
 # In[29]:
 
@@ -197,8 +182,7 @@ def assignBin (x):
 	binsize = 30
 	return int(x / binsize)
 
-summary['age'] = summary['age'].apply(assignBin)
-maxBin = max(summary['age'].values) + 1
+
 # one hot encoding 
 def age2Bin(x): # 0 if row['gender']=='male' else 1
 	vector = [0] * maxBin
@@ -206,56 +190,59 @@ def age2Bin(x): # 0 if row['gender']=='male' else 1
 	del vector[-1]
 	return vector 
 
-summary.ethnicity = pd.Categorical(summary.ethnicity)
-summary.ethnicity = summary.ethnicity.cat.codes
-maxBin2 = max(summary['ethnicity'].values) + 1
+
 def eth2Bin(x):
 	vector = [0] * maxBin2
 	vector[x] = 1
 	del vector[-1]
 	return vector 
 
+def generateSoftMax(train):
+	# convert training data 
+	print('Collect training data...')
+	trainx = []
+	atrainy = []
+	gtrainy = []
+	etrainy = []
 
+	for index, row in train.iterrows():
+		img = pictures[row['filename']]
+		if not isinstance(img, str):
+			trainx.append(img)
+			atrainy.append(age2Bin(row['age']))
+			gtrainy.append(0 if row['gender']=='male' else 1)
+			etrainy.append(eth2Bin(row['ethnicity']))
+	trainx = np.array(trainx)
+	atrainy = np.array(atrainy)
+	gtrainy = np.array(gtrainy).reshape(-1,1)
+	etrainy = np.array(etrainy)
+	trainy = {
+		"age": atrainy, 
+		"gender": gtrainy, 
+		"ethnicity": etrainy
+	}
+	return trainx, trainy
 
-epochcnt = 80 # int(sys.argv[1] )
-batch = 16 # int(sys.argv[2] )
+# calculate results 
+def generateReport(testx, testy):
+	results = ''
+	(apredy, gpredy, epredy) = model.predict(testx)
+	for i, (y_test, y_pred) in enumerate([(testy['age'], apredy), (testy['gender'], gpredy), (testy['ethnicity'], epredy)]):
+		matrix = confusion_matrix(y_test.argmax(axis=1).reshape(-1,1), y_pred.argmax(axis=1).reshape(-1,1))
+		b = '\n'.join('\t'.join('%0.3f' %x for x in y) for y in matrix)
+		results += '{}-{}\n\n{}\n\n\n'.format(i, matrix.shape, b)
+	return results
 
-# split into train and test data 
-trainlen = split =  int(summary.shape[0] * 0.8)
-summary = summary.sample(frac=1).reset_index(drop=True) # shuffle database rows in place
-train = summary.iloc[:split]
-test = summary.iloc[split:]
-
-# convert training data 
-print('Collect training data...')
-trainx = []
-atrainy = []
-gtrainy = []
-etrainy = []
-
-for index, row in train.iterrows():
-	img = pictures[row['filename']]
-	if not isinstance(img, str):
-		trainx.append(img)
-		atrainy.append(age2Bin(row['age']))
-		gtrainy.append(0 if row['gender']=='male' else 1)
-		etrainy.append(eth2Bin(row['ethnicity']))
-trainx = np.array(trainx)
-atrainy = np.array(atrainy)
-gtrainy = np.array(gtrainy).reshape(-1,1)
-etrainy = np.array(etrainy)
-trainy = {
-	"age": atrainy, 
-	"gender": gtrainy, 
-	"ethnicity": etrainy
-}
-
-print('ratio: {}'.format(float(sum(gtrainy)) / len(gtrainy)))
-
-
-print('build model...')
-for PreBuilt, model_name in [(VGG16, 'VGG16')]:#, (ResNet152V2, 'ResNet152V2')]:
-	start2 = time.time()
+def saveModel(model):
+	# serialize model to JSON
+	model_json = model.to_json()
+	with open("total model-{}-{}-{}.json".format(trainlen, epochcnt, batch), "w") as json_file:
+		json_file.write(model_json)
+	# serialize weights to HDF5
+	model.save_weights("total model-{}-{}-{}.h5".format(trainlen, epochcnt, batch))
+	print("Saved model to disk")
+	
+def buildModel():
 	model = PreBuilt(
 		include_top=True,
 		weights="imagenet",
@@ -283,65 +270,60 @@ for PreBuilt, model_name in [(VGG16, 'VGG16')]:#, (ResNet152V2, 'ResNet152V2')]:
 		"ethnicity": "binary_crossentropy",
 	}
 
-
 	model.compile(optimizer='Adam', loss=losses, metrics=['accuracy'])
-
-	# train model 
-	print('train model...')
-	start = time.time()
-	model.fit(trainx, trainy, epochs=epochcnt, batch_size=batch) 
-	print('time taken: {} minutes'.format((time.time() - start)/60))
-
-	# serialize model to JSON
-	model_json = model.to_json()
-	with open("total model-{}-{}-{}.json".format(trainlen, epochcnt, batch), "w") as json_file:
-		json_file.write(model_json)
-	# serialize weights to HDF5
-	model.save_weights("total model-{}-{}-{}.h5".format(trainlen, epochcnt, batch))
-	print("Saved model to disk")
-
-
-	print('Collect testing data...')
-	trainx = []
-	atrainy = []
-	gtrainy = []
-	etrainy = []
-
-	for index, row in test.iterrows():
-		img = pictures[row['filename']]
-		if not isinstance(img, str):
-			trainx.append(img)
-			atrainy.append(age2Bin(row['age']))
-			gtrainy.append(0 if row['gender']=='male' else 1)
-			etrainy.append(eth2Bin(row['ethnicity']))
-	trainx = np.array(trainx)
-	atrainy = np.array(atrainy)
-	gtrainy = np.array(gtrainy).reshape(-1,1)
-	etrainy = np.array(etrainy)
-	print('ratio: {}'.format(float(sum(gtrainy)) / len(gtrainy)))
-
-	trainy = {
-		"age": atrainy, 
-		"gender": gtrainy, 
-		"ethnicity": etrainy
-	}
-
-	# calculate results 
-	(apredy, gpredy, epredy) = model.predict(trainx)
-	results = ''
-	for i, y_test, y_pred in enumerate([(atrainy, apredy), (gtrainy, gpredy), (etrainy, epredy)]):
-		matrix = confusion_matrix(y_test.argmax(axis=1), y_pred.argmax(axis=1))
-		b = '\n'.join('\t'.join('%0.3f' %x for x in y) for y in matrix)
-		results += '{}\n\n{}\n\n\n'.format(i, b)
-	
+	return model 
 	
 	# print('Test Accuracy: {}'.format())
 	# scores = model.evaluate(trainx, trainy)
 	# confusion_matrix(y_test, y_pred)
-	print('total time taken: {} minutes'.format((time.time() - startx)/60))
+	
 	# sys.stdout = notify.log
 	#string = '{}\n'.format(time.time() - start2)
 	#for i in range(len(model.metrics_names)):
 	#	string += "%s: %.2f%%\n" % (model.metrics_names[i], scores[i]*100)
-	notify.mail('Model Finished: ' + model_name, results)
+	
+if __name__ == '__main__':
+	summary = importData('./part3.tar/')
+	detector = MTCNN()
+	# print(summary)
+	# print ('-'*80)
+	cropFaces(summary)
+		
+	del detector
+	#K.clear_session()
+	#gc.collect()
+	summary['age'] = summary['age'].apply(assignBin)
+	maxBin = max(summary['age'].values) + 1
 
+	summary.ethnicity = pd.Categorical(summary.ethnicity)
+	summary.ethnicity = summary.ethnicity.cat.codes
+	maxBin2 = max(summary['ethnicity'].values) + 1
+
+	epochcnt = 40 # int(sys.argv[1] )
+	batch = 16 # int(sys.argv[2] )
+
+	# split into train and test data 
+	trainlen = split =  int(summary.shape[0] * 0.8)
+	summary = summary.sample(frac=1).reset_index(drop=True) # shuffle database rows in place
+	train = summary.iloc[:split]
+	test = summary.iloc[split:]
+	trainx, trainy = generateSoftMax(train)
+	print('ratio: {}'.format(float(sum(trainy['gender'])) / len(trainy['gender'])))
+	
+	print('build model...')
+	for PreBuilt, model_name in [(VGG16, 'VGG16')]:#, (ResNet152V2, 'ResNet152V2')]:
+		start2 = time.time()
+		model = buildModel()
+
+		# train model 
+		print('train model...')
+		start = time.time()
+		model.fit(trainx, trainy, epochs=epochcnt, batch_size=batch) 
+		print('time taken: {} minutes'.format((time.time() - start)/60))
+
+		model.save ('testmodel') # saveModel(model)
+		testx, testy = generateSoftMax(test)
+		generateReport(testx, testy)
+	
+	print('total time taken: {} minutes'.format((time.time() - startx)/60))
+	notify.mail('Model Finished: ' + model_name, results)
